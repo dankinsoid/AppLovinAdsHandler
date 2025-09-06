165a6fc38a10f6f3762da0a86f8c2efd24c0276b
Danil <voidilov@gmail.com>
2025-09-06 19:45:28 +0200

    1.0.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/dankinsoid/swift-ads.git", from: "1.0.1"),
+        .package(url: "https://github.com/dankinsoid/swift-ads.git", from: "1.0.2"),
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: "13.0.0"),
     ],
     targets: [
